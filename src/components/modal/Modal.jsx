fc03427956d02f9d136407cb1b968800d81f852e
kulikiliya <136320307+kulikiliya@users.noreply.github.com>
2023-09-19 16:57:42 +0300

    tryagain

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,36 +1,6 @@
-import React, { Component, useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { ModalOverlay, ModalDiv } from './Modal.Styled';
 import PropTypes from 'prop-types';
-
-// class Modal extends Component {
-//   onBackDropClick = e => {
-//     if (e.currentTarget === e.target || e.key === 'Escape') {
-//       this.props.close();
-//     }
-//   };
-
-//   handleKeyDown = e => {
-//     if (e.key === 'Escape' && this.props.isOpen) {
-//       this.props.close();
-//     }
-//   };
-
-//   componentDidMount() {
-//     document.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     document.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   render() {
-//     return (
-//       <ModalOverlay className="overlay" onClick={this.onBackDropClick}>
-//         <ModalDiv className="modal">{this.props.children}</ModalDiv>
-//       </ModalOverlay>
-//     );
-//   }
-// }
 
 export const Modal = ({ close, isOpen, children }) => {
   const onBackDropClick = e => {
